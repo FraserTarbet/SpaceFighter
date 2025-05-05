97385f5a3ef5fc9090b7029ce0dd14caeab64759
Fraser Tarbet <frasertarbet@hotmail.com>
2025-05-05 19:12:51 +0100

    control rotation

--- scripts/control.gd
+++ scripts/control.gd
@@ -3,8 +3,16 @@ extends Node2D
 var touch_velocities = {}
 var touch_positions = {}
 var velocity_smoothing = 2
+var rotation_smooting = 3
 var min_detected_velocity = 100
 var single_touch_proximity = 50
+var angle_delta_to_torque = 5000
+var rotation_node = null
+var last_angle = null
+var last_time = null
+var angular_velocity_samples = []
+var max_linear_velocity = 500
+var max_angular_velocity = 90
 
 func _input(event):
     if event is InputEventScreenTouch:
@@ -16,7 +24,12 @@ func _input(event):
         elif not event.pressed and touch_velocities.has(event.index):
             touch_velocities.erase(event.index)
             touch_positions.erase(event.index)
-            Globals.player_ship.control_velocity = null
+            angular_velocity_samples = []
+            rotation_node = null
+            last_angle = null
+            last_time = null
+            Globals.player_ship.control_linear_velocity = null
+            Globals.player_ship.control_angular_velocity = null
 
     if event is InputEventScreenDrag and touch_positions.has(event.index) and event.velocity.length() >= min_detected_velocity:
         touch_positions[event.index] = event.position
@@ -25,7 +38,31 @@ func _input(event):
         touch_velocities[event.index].append(event.velocity)
 
         var smooth_velocity = touch_velocities[event.index].reduce(func(accum, _nothing): return accum / len(touch_velocities[event.index]))
+        if smooth_velocity.length() > max_linear_velocity:
+            smooth_velocity = smooth_velocity.normalized() * max_linear_velocity
+        Globals.player_ship.control_linear_velocity = smooth_velocity
 
-        Globals.player_ship.control_velocity = smooth_velocity
+        # Rotation
+        if len(touch_positions) == 2:
+            if rotation_node == null:
+                rotation_node = Node2D.new()
+            rotation_node.position = (touch_positions.values()[0] + touch_positions.values()[1]) / 2
+            rotation_node.look_at(touch_positions.values()[1])
 
-        print(event.velocity)
+            if last_angle != null and (Time.get_ticks_msec()/1000.0 - last_time/1000.0) > 0.0:
+                var angle_delta = wrapf(rotation_node.rotation - last_angle, -PI, PI)
+                var angular_velocity = (angle_delta * angle_delta_to_torque) / (Time.get_ticks_msec()/1000.0 - last_time/1000.0)
+
+                if len(angular_velocity_samples) > rotation_smooting:
+                    angular_velocity_samples.remove_at(0)
+                angular_velocity_samples.append(angular_velocity)
+
+                var smooth_angular_velocity = angular_velocity_samples.reduce(func(accum, _nothing): return accum / len(angular_velocity_samples))
+                Globals.player_ship.control_angular_velocity = clampf(smooth_angular_velocity, -max_angular_velocity, max_angular_velocity)
+
+            last_angle = rotation_node.rotation
+            last_time = Time.get_ticks_msec()
+        else:
+            rotation_node = null
+            last_angle = null
+            last_time = null
--- scripts/ship_player.gd
+++ scripts/ship_player.gd
@@ -1,10 +1,13 @@
 extends Ship
 
-var control_velocity = null
+var control_linear_velocity = null
+var control_angular_velocity = null
 
 func _ready():
     Globals.player_ship = self
 
 func _physics_process(_delta):
-    if control_velocity != null:
-        apply_force(control_velocity)
+    if control_linear_velocity != null:
+        apply_force(control_linear_velocity)
+    if control_angular_velocity != null:
+        apply_torque(control_angular_velocity)
