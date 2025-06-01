553dc41e0be0e1af03130a67dbb022e30abead0d
Fraser Tarbet <frasertarbet@hotmail.com>
2025-06-01 15:58:51 +0100

    web sound workaround

--- scripts/SampleManager.gd
+++ scripts/SampleManager.gd
@@ -12,27 +12,53 @@ var rng = RandomNumberGenerator.new()
 func _ready():
     Globals.sample_manager = self
 
-    # Register samples
-    var path = "res://sounds"
-    var dir = DirAccess.open(path)
-    dir.list_dir_begin()
-    var file = dir.get_next()
-    while file != "":
-        if file.ends_with(".wav"):
-            var res = ResourceLoader.load(path + "/" + file)
-            AudioServer.register_stream_as_sample(res)
-            var bank = file.substr(0, file.rfind("_"))
-            if not sample_banks.keys().has(bank):
-                sample_banks[bank] = []
-            sample_banks[bank].append(res)
-        file = dir.get_next()
+    # # Register samples
+    # var path = "res://sounds"
+    # var dir = DirAccess.open(path)
+    # dir.list_dir_begin()
+    # var file = dir.get_next()
+    # while file != "":
+    #     if file.ends_with(".wav"):
+    #         var stream: AudioStream = ResourceLoader.load(path + "/" + file)
+    #         AudioServer.register_stream_as_sample(stream)
+    #         var bank = file.substr(0, file.rfind("_"))
+    #         if not sample_banks.keys().has(bank):
+    #             sample_banks[bank] = []
+    #         sample_banks[bank].append(stream)
+    #     file = dir.get_next()    
+
+    # Create sample bank manually - web workaround
+    sample_banks = {
+        'explosion': [
+            ResourceLoader.load("res://sounds/explosion_0.wav"),
+            ResourceLoader.load("res://sounds/explosion_1.wav"),
+            ResourceLoader.load("res://sounds/explosion_2.wav"),
+            ResourceLoader.load("res://sounds/explosion_3.wav"),
+            ResourceLoader.load("res://sounds/explosion_4.wav")
+        ],
+        'shell_fire': [
+            ResourceLoader.load("res://sounds/shell_fire_0.wav")
+        ],
+        'shell_hit': [
+            ResourceLoader.load("res://sounds/shell_hit_0.wav"),
+            ResourceLoader.load("res://sounds/shell_hit_1.wav"),
+            ResourceLoader.load("res://sounds/shell_hit_2.wav"),
+            ResourceLoader.load("res://sounds/shell_hit_3.wav"),
+            ResourceLoader.load("res://sounds/shell_hit_4.wav")
+        ]
+    }
+
+    # Register streams
+    for bank in sample_banks.keys():
+        for stream in sample_banks[bank]:
+            AudioServer.register_stream_as_sample(stream)
 
     # Create players
     for i in range(player_count):
         var player = SamplePlayer.new()
         player.manager = self
         free_players.append(player)
-        add_child(player)
+        add_child(player)    
 
 func play_sample_at(sample_bank_name: String, at: Vector2, level: float = 0.0):
     if free_players.size() == 0:
--- scripts/SamplePlayer.gd
+++ scripts/SamplePlayer.gd
@@ -5,4 +5,5 @@ extends AudioStreamPlayer2D
 var manager
 
 func _ready():
-	finished.connect(manager.free_player.bind(self))
+	if manager:
+		finished.connect(manager.free_player.bind(self))
